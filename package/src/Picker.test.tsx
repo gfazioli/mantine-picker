267180994732e540d84c90020678fa9746837a3c
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-04-15 11:56:17 +0200

    ✅ Updated

--- package/src/Picker.test.tsx
+++ package/src/Picker.test.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import { render, tests } from '@mantine-tests/core';
-import { Picker, PickerProps, PickerStylesNames } from './Picker';
-
-const defaultProps: PickerProps = {
-  data: [],
-};
+import { render } from '@mantine-tests/core';
+import { Picker } from './Picker';
 
 describe('@mantine/core/Picker', () => {
-  tests.itSupportsSystemProps<PickerProps, PickerStylesNames>({
-    component: Picker,
-    props: defaultProps,
-    styleProps: true,
-    children: true,
-    classes: true,
-    id: true,
-    refType: HTMLDivElement,
-    displayName: '@mantine/core/Picker',
-    stylesApiSelectors: ['root'],
-  });
-
   it('supports perspective prop', () => {
-    const { container } = render(<Picker data={[]} />);
-    expect(container.querySelector('.mantine-Picker-root')).toHaveStyle({ perspective: '500px' });
+    const { container } = render(<Picker data={[]} perspective={500} />);
+    expect(container.querySelector('.mantine-Picker-root')).toHaveStyle({
+      '--picker-perspective': '500px',
+    });
   });
 });
